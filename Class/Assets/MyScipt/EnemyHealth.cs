f61373b262937a39527e891754e87e40ab1a119d
IamSloth <deathbed0104@gmail.com>
2022-05-12 19:51:02 +0900

    First Class

---
+++ Class/Assets/MyScipt/EnemyHealth.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    int hp = 100;
+
+    public void Damage(int amout)
+    {
+        if (hp <= 0)
+            return;
+        hp -= amout;
+        if(hp <= 0)
+        {
+            GetComponent<Animator>().SetTrigger("Death");
+            GetComponent<NavMeshAgent>().enabled = false;
+
+            Destroy(gameObject, 2);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
